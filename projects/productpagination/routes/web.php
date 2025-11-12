7b784e7c5bacb7edecd4b49d2cf9582ff0efb95c
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2025-11-12 08:00:47 +0700

    add: modul8

--- projects/productpagination/routes/web.php
+++ projects/productpagination/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
